f2a281a30383f19f8c175ef770ac0f0184710445
sasodoma <saso.doma@gmail.com>
2021-08-19 22:43:26 +0200

    Implemented period synchronisation
    Works kinda OK while the mains is connected,
    but when disconnecting (perhaps to move to
    another outlet) it quickly looses sync.

--- phase_checker/phase_checker.ino
+++ phase_checker/phase_checker.ino
@@ -5,20 +5,41 @@ void setup() {
 }
 
 void loop() {
-  static unsigned long lastMillis = 0;
+  // Duration of one mains period in MCU microseconds (different from real time)
+  static uint16_t period = 20000;
+  static unsigned long cumulative_zero = 0;
+  static unsigned long last_pulse = 0;
+  
+  static unsigned long last_millis = 0;
   static boolean input_was_high = true;
-  if (millis() - lastMillis > 3) {
+  // Update our relative zero time
+  if (micros() - cumulative_zero > period) cumulative_zero += period;
+  
+  if (millis() - last_millis > 3) {
     if (digitalRead(PB0)) {
       if (!input_was_high) {
-        lastMillis = millis();
+        last_millis = millis();
         input_was_high = true;
       }
     } else {
       if (input_was_high) {
-        lastMillis = millis();
-        uint16_t phase_time = micros() % 20000;
-        Serial.print(map_float(phase_time, 0, 20000, 0, 360));
-        Serial.println("°");
+        // Get both times at moment of pulse
+        unsigned long micros_now = micros();
+        last_millis = millis();
+        // Calculate period
+        unsigned long last_period = micros_now - last_pulse;
+        // Save this pulse's time
+        last_pulse = micros_now;
+
+        // Update period
+        period += last_period > period;
+        period -= last_period < period;
+
+        // Get phase relative to our current zero.
+        uint16_t phase_time = (micros_now - cumulative_zero) % period;
+        Serial.print(map_float(phase_time, 0, period, 0, 360));
+        Serial.print("°, Period: ");
+        Serial.println(period);
       }
       input_was_high = false;
     }
